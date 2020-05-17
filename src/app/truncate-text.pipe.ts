e22b70a0fb6b2356b5131cea6aff298e06c49433
Quyen Tran <tranngtuquyen@gmail.com>
2020-05-16 19:53:56 -0700

    Create new truncateText pipe

--- src/app/truncate-text.pipe.ts
+++ src/app/truncate-text.pipe.ts
@@ -5,8 +5,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncateTextPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, length: number): string {
+    const biggestWord = 50;
+    const elipses = "...";
+ 
+    if(typeof value === "undefined") return value;
+    if(value.length <= length) return value;
+ 
+    //.. truncate to about correct lenght
+    let truncatedText = value.slice(0, length + biggestWord);
+ 
+    //.. now nibble ends till correct length
+    while (truncatedText.length > length - elipses.length) {
+ 
+        let lastSpace = truncatedText.lastIndexOf(".");
+        if(lastSpace === -1) break;
+        truncatedText = truncatedText.slice(0, lastSpace).replace(/[!,.?;:]$/, '');
+ 
+    };
+ 
+   return truncatedText + elipses;
   }
 
 }
